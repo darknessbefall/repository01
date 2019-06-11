b2107dc3b000e022a2270224fad905ba8bad80d8
Eren <850613336@qq.com>
2019-06-11 19:16:04 +0800

    第二次修改,创建了一个方法.

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,9 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("HelloWorld!");
 		System.out.println("第一次修改");
+		this.sec();
 	}
+	public static void sec(){
+		System.out.println("第三次修改");
+	} 
 }
